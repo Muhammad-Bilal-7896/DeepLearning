19a701a655ffe38e1fb1c44d3729c67f97d856c0
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-04-12 05:55:58 +0500

    feat

--- TensorFlow.React.js/first_simple_tensor_flow_web_app/pages/_app.js
+++ TensorFlow.React.js/first_simple_tensor_flow_web_app/pages/_app.js
@@ -1,7 +1,31 @@
+import Head from "next/head";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="description" content="DeepLearningWork" />
+      <meta name="keywords" content="AI,deeplearning,js,tensorflow" />
+      <meta name="author" content="Muhammad-Bilal-7896" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      
+      <title>Deep learning with tensorflow</title>
+    </Head>
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  </>
 }
 
 export default MyApp
